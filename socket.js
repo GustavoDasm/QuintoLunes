afc08381f69c1f0e7ae4838e42ad6082ff8f4adf
GustavoDASM <animex36990@gmail.com>
2025-05-06 09:15:10 -0500

    socket mensajes

--- socket.js
+++ socket.js
@@ -1,8 +1,25 @@
 module.exports = (io) => {
+    /*     io.on('connection', (socket) => {
+            console.log('Se ha conectado a un nuevo cliente al servidor')
+            socket.on('disconect', () => {
+                console.log('El cliente se ha desconectado del servidor')
+            })
+        }) */
+
+    let messages = [{ username: "fake-user", message: "welcome" }]
+
     io.on('connection', (socket) => {
-        console.log('Se ha conectado a un nuevo cliente al servidor')
-        socket.on('disconect', () => {
-            console.log('El cliente se ha desconectado del servidor')
+        console.log("Un nuevo usuario conectado")
+        io.emit('all-messages', messages)
+
+        socket.on('writing', (username) => {
+            socket.broadcast.emit('writing', username)
         })
+
+        socket.on('new-message', (data) => {
+            messages.push(data)
+            io.emit('all-messages', messages)
+        })
+
     })
 }
